d53b7e6f04d8eb92a152b85b6539d3045c0ed098
kahstan <kahsheng@gmail.com>
2022-09-28 14:28:56 +0800

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,9 +18,7 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
-          <p className="py-4">
-            // These are the technologies I've worked with.
-          </p>
+          <p className="py-4">These are the technologies I've worked with.</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
@@ -51,6 +49,10 @@ const Skills = () => {
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
             <p className="my-4">MONGO DB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">TAILWIND CSS</p>
           </div>
         </div>
       </div>
