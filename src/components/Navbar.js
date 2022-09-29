2f9ad396bd65fc284fd1ab2e0d708d85bc937c3a
kahstan <kahsheng@gmail.com>
2022-09-29 15:40:36 +0800

    hover update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,27 +16,27 @@ const Navbar = () => {
 
       {/* menu */}
       <ul className="hidden md:flex">
-        <li className="hover:border-b-4 border-pink-600">
+        <li className="hover:border-b-4 border-[#00df9a]">
           <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li>
+        <li className="hover:border-b-4 border-[#00df9a]">
           <Link to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li>
+        <li className="hover:border-b-4 border-[#00df9a]">
           <Link to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li>
+        <li className="hover:border-b-4 border-[#00df9a]">
           <Link to="work" smooth={true} duration={500}>
             Projects
           </Link>
         </li>
-        <li>
+        <li className="hover:border-b-4 border-[#00df9a]">
           <Link to="contact" smooth={true} duration={500}>
             Contact
           </Link>
