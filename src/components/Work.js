acbe16aae906e946f4a1a74e8ae49f711edac14e
kahstan <kahsheng@gmail.com>
2022-09-29 16:01:57 +0800

    added burger assembly

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,7 @@ import React from "react";
 import Screenshot_Lyrical from "../assets/Screenshot_Lyrical.png";
 import Screenshot_Snakes from "../assets/Screenshot_Snakes.png";
 import Screenshot_Pawesome from "../assets/Screenshot_Pawesome.png";
+import Screenshot_BA from "../assets/Screenshot_BA.png";
 
 const Work = () => {
   return (
@@ -87,14 +88,14 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* <div
-            style={{ backgroundImage: `url(${Screenshot_Snakes})` }}
+          <div
+            style={{ backgroundImage: `url(${Screenshot_BA})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          > */}
-          {/* Hover Effects */}
-          {/* <div className="opacity-0 group-hover:opacity-100">
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                Burger Assembly
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
@@ -102,14 +103,14 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Kahstan/Burger-Assembly">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
               </div>
             </div>
-          </div> */}
+          </div>
           {/* Grid Item */}
           {/* <div
             style={{ backgroundImage: `url(${Screenshot_Lyrical})` }}
