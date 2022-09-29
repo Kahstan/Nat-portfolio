e93334f479dfc07be462411e4c428bdf1d8e942a
kahstan <kahsheng@gmail.com>
2022-09-29 15:28:14 +0800

    color

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,7 @@ import Screenshot_Pawesome from "../assets/Screenshot_Pawesome.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div name="work" className="w-full md:h-screen bg-gray-100 text-gray-900">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
